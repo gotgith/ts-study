0ce3b604d3cc67559a4fceb1e1df277e0a785175
JH <18468107890@163.com>
2020-06-08 15:01:42 +0800

    泛型约束

--- 泛型.ts
+++ 泛型.ts
@@ -4,3 +4,19 @@ function add<T>(a: any, b: any): T {
 }
 add<number>(1, 2)
 add<string>("12", "34")
+
+// function returnIt<T>(arg: T): T{
+//     console.log(arg.length) // error
+//     return arg;
+// }
+
+
+// 泛型约束
+interface HasLength {
+    length: number
+}
+
+function returnIt<T extends HasLength>(arg: T): T {
+    console.log(arg.length)
+    return arg;
+}
